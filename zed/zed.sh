2272f44806ef76099d902444471fdea1218684d6
aalniak <arda.alniak@metu.edu.tr>
2025-11-09 02:02:50 +0200

    mount entire config folder

--- zed/zed.sh
+++ zed/zed.sh
@@ -18,13 +18,11 @@ docker run --runtime=nvidia --rm \
   -v /usr/local/zed/resources:/usr/local/zed/resources \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /dev:/dev \
-  -v /media/SSD/zed/zed-ros2-wrapper/:/zed_dyna \
+  -v /media/SSD/zed/zed-ros2-wrapper/zed_wrapper/config/:"/root/ros2_ws/install/zed_wrapper/share/zed_wrapper/config/" \
   zed_ros2_l4t_36.4.0_sdk_5.1.0 \
   bash -lc "source /opt/ros/humble/install/setup.bash && \
             source /root/ros2_ws/install/setup.bash && \
             export ROS_DOMAIN_ID=42 && \
             export RMW_IMPLEMENTATION=rmw_fastrtps_cpp && \
             export ROS_LOCALHOST_ONLY=0 && \
-            cp /zed_dyna/zed_wrapper/config/common_stereo.yaml ./src/zed_wrapper/config/common_stereo.yaml && \
-            cp /zed_dyna/zed_wrapper/config/zed2i.yaml ./src/zed_wrapper/config/zed2i.yaml && \
             ros2 launch zed_wrapper zed_camera.launch.py camera_model:=zed2i"
