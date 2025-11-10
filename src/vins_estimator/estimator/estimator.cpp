d26fb8ed9c9dbbd730af80c927ea38f4b68a9ce6
aalniak <arda.alniak@metu.edu.tr>
2025-11-10 18:19:19 +0200

    remove debug visualizations

--- src/vins_estimator/estimator/estimator.cpp
+++ src/vins_estimator/estimator/estimator.cpp
@@ -159,10 +159,11 @@ void Estimator::changeSensorType(int use_imu, int use_stereo) {
 void Estimator::inputImage(double t, const cv::Mat &_img, const cv::Mat &depth_img, 
                            const cv::Mat &_img1) {
   inputImageCnt++;
-
+  
   // A visualization to debug
   if (!depth_img.empty())
   {
+      std::cout << "Depth image acquired successfully!" << std::endl;
       // Normalize the 32FC1 float image to 0-255 (8UC1)
       cv::Mat viz_depth;
       cv::normalize(depth_img, viz_depth, 0, 255, cv::NORM_MINMAX, CV_8UC1);
@@ -172,10 +173,10 @@ void Estimator::inputImage(double t, const cv::Mat &_img, const cv::Mat &depth_i
       cv::applyColorMap(viz_depth, color_depth, cv::COLORMAP_INFERNO);
 
       // Show the image in a window
-      cv::imshow("Depth Map", color_depth);
-      
-      // We can also show the original RGB image to compare
-      cv::imshow("Input Image", _img);
+      //cv::imshow("Depth Map", color_depth);
+      //
+      //// We can also show the original RGB image to compare
+      //cv::imshow("Input Image", _img);
       
       // 4. IMPORTANT: Wait 1ms for OpenCV to process GUI events
       cv::waitKey(1);
